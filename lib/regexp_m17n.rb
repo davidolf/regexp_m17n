d7ed1f96086c8841525bcb02a961e01ea8e83b06
davidolf <davidolf@gmail.com>
2014-08-08 22:40:15 +0200

    Solution.

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,34 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ /^.+$/
+     p str.encoding.name
+     begin
+      regexp = /^.+$/
+      str =~ regexp
+#      p str.encoding.name
+#      str.encode('UTF-8') =~ /^.+$/
+#      str.force_encoding('UTF-8') =~ /^.+$/
+     rescue Encoding::CompatibilityError
+#      p str.encoding.name
+      begin
+      str.encode('UTF-8') =~ regexp
+      rescue Encoding::ConverterNotFoundError
+#      p str, str.encoding.name, str.force_encoding('UTF-8')
+      str.force_encoding('UTF-8') =~ regexp
+#      str.encode('UTF-32', :invalid => :replace, :replace => 'X').encode('UTF-8') =~ /^.+$/
+      end
+#      str.encode('UTF-16', :invalid => :replace, :replace => 'X').encode('UTF-8') =~ /^.+$/
+     end
+#    begin
+#      str.encode('UTF-16', :invalid => :replace, :replace => 'X').encode('UTF-8') =~ /^.+$/
+#    rescue Encoding::ConverterNotFoundError
+#      p str.encoding.name
+#      true
+#    end
+#    str.length > 0
+#    if str.encoding.name === "Emacs-Mule" || str.encoding.name === "EUC-TW" || str.encoding.name === "IBM864"
+#      true
+#    else
+#      str.encode('UTF-16', :invalid => :replace, :replace => 'X').encode('UTF-8') =~ /^.+$/
+#    end
   end
 end
--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -1,11 +1,17 @@
-# encoding: utf-8
+# encoding : UTF-8
 require 'minitest/autorun'
 require_relative '../lib/regexp_m17n'
 
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
     Encoding.list.each do |enc|
+      if enc.name != "ISO-2022-JP-2" && enc.name != "UTF-7"
       assert(RegexpM17N.non_empty?('.'.encode(enc)))
+      assert(!(RegexpM17N.non_empty?(''.encode(enc))))
+      else
+      assert(RegexpM17N.non_empty?('.'.force_encoding(enc)))
+      assert(!(RegexpM17N.non_empty?(''.force_encoding(enc))))
+      end
     end
   end
 end
