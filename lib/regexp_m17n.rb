4101d3c1f5aaa8af0c105068dab4d85c7ea6274a
davidolf <davidolf@gmail.com>
2014-08-08 22:48:30 +0200

    Polished solution...

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,34 +1,15 @@
 module RegexpM17N
   def self.non_empty?(str)
-     p str.encoding.name
+#     p str.encoding.name
      begin
-      regexp = /^.+$/
-      str =~ regexp
-#      p str.encoding.name
-#      str.encode('UTF-8') =~ /^.+$/
-#      str.force_encoding('UTF-8') =~ /^.+$/
+       regexp = /^.+$/
+       str =~ regexp
      rescue Encoding::CompatibilityError
-#      p str.encoding.name
-      begin
-      str.encode('UTF-8') =~ regexp
-      rescue Encoding::ConverterNotFoundError
-#      p str, str.encoding.name, str.force_encoding('UTF-8')
-      str.force_encoding('UTF-8') =~ regexp
-#      str.encode('UTF-32', :invalid => :replace, :replace => 'X').encode('UTF-8') =~ /^.+$/
-      end
-#      str.encode('UTF-16', :invalid => :replace, :replace => 'X').encode('UTF-8') =~ /^.+$/
+       begin
+         str.encode('UTF-8') =~ regexp
+       rescue Encoding::ConverterNotFoundError
+         str.force_encoding('UTF-8') =~ regexp
+       end
      end
-#    begin
-#      str.encode('UTF-16', :invalid => :replace, :replace => 'X').encode('UTF-8') =~ /^.+$/
-#    rescue Encoding::ConverterNotFoundError
-#      p str.encoding.name
-#      true
-#    end
-#    str.length > 0
-#    if str.encoding.name === "Emacs-Mule" || str.encoding.name === "EUC-TW" || str.encoding.name === "IBM864"
-#      true
-#    else
-#      str.encode('UTF-16', :invalid => :replace, :replace => 'X').encode('UTF-8') =~ /^.+$/
-#    end
   end
 end
